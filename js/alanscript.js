b57d0983246d53f5558b59368daee7a96312d465
Aditya Vikram Kochar <adityakochar9@gmail.com>
2022-05-29 04:10:01 +0530

    3rd chkpoint

---
+++ js/alanscript.js
@@ -0,0 +1,9 @@
+var alanBtnInstance = alanBtn({
+    key: "ca9e289fb7fbfa46e857c35e774a78082e956eca572e1d8b807a3e2338fdd0dc/stage",
+    onCommand: function (commandData) {
+      if (commandData.command === "go:back") {
+        //call client code that will react on the received command
+      }
+    },
+    rootEl: document.getElementById("alan-btn"),
+  });
